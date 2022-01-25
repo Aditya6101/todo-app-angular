814a9509652a49457179b721abe1239b8494457d
Aditya Kamble <adityakamble6101@gmail.com>
2022-01-25 19:35:00 +0530

    new components and module added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ThemeSwitcherComponent } from './components/theme-switcher/theme-switcher.component';
 import { AddTodoComponent } from './components/add-todo/add-todo.component';
 import { FormsModule } from '@angular/forms';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
@@ -13,8 +16,10 @@ import { FormsModule } from '@angular/forms';
     HeaderComponent,
     ThemeSwitcherComponent,
     AddTodoComponent,
+    TodoListComponent,
+    TodoItemComponent,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
